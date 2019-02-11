c17b79a8f09ee13773d6c30934d1c0e4b78ea230
marcin.cyboran <marcin.cyboran.wsb@gmail.com>
2019-02-11 17:43:58 +0100

    fast1

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+const winston = require('winston');
 const express = require('express');
 
 const app = express();
+
 require('./startup/loggin')();
 require('./startup/config')();
+require('./startup/validation')();
 require('./startup/routes')(app);
 require('./startup/connect')();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is up. Listening on port ${port}...`)
+  console.log(`Server is up. Listening on port ${port}...`);
+  winston.info(`Server is up. Listening on port ${port}...`);
 });
--- startup/config.js
+++ startup/config.js
@@ -3,7 +3,6 @@ const config = require('config');
 module.exports = function () {
     if (!config.get('jwtPrivateKey')) {
         console.log('ERROR - projekt1_jwtPrivateKey is not defined');
-        winston.error('ERROR - projekt1_jwtPrivateKey is not defined');
-        process.exit(1);
+        throw new Error(`ERROR - projekt1_jwtPrivateKey is not defined`)
     }
 };
